141da7b9359cc8f26f5480005b55cf013256ba61
keveightysev <keveightysev@gmail.com>
2019-04-08 15:12:20 -0500

    Repair test passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,20 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+const testObject = {
+  name: 'Kevin',
+  durability: 50,
+  enhancement: 10,
+};
+
+describe('enhancer.js', () => {
+  describe('repair()', () => {
+    it('should restore durability to 100', () => {
+      expect(enhancer.repair(testObject)).toEqual(
+        expect.objectContaining({
+          durability: 100,
+        }),
+      );
+    });
+  });
+});
